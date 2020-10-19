a463014c43c11f3af440124c8d5f62b72fdaa041
kuromadara <dowarah.dayananda35@gmail.com>
2020-10-19 14:06:13 +0530

    multi-lingual download status clear button fix

--- app/src/main/java/com/example/handwrittingrecognition/MainActivity.kt
+++ app/src/main/java/com/example/handwrittingrecognition/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.handwrittingrecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    public lateinit var editText: EditText
 
 
 
@@ -15,27 +14,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val languageMapToLangCode: HashMap<String, String> = hashMapOf("English" to "en", "Assamese" to "as", "Hindi" to "hi")
+        val languages = resources.getStringArray(R.array.Languages)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, languages
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    StrokeManager.lang = languageMapToLangCode[languages.get(position)].toString()
+                    StrokeManager.download(this@MainActivity)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
 
 
-        StrokeManager.download()
+        var editText = findViewById(R.id.editTextText) as EditText
+
+        StrokeManager.download(this@MainActivity)
         recognize.setOnClickListener {
 
             recognize.isClickable = false
 
             StrokeManager.recognize(this){result ->
                 recognize.isClickable = true
-
-                editText = findViewById(R.id.editTextText) as EditText
                 editText.setText(result)
+
             }
 
         }
 
 
         clear.setOnClickListener {
+
             drawingView.clear()
             StrokeManager.clear()
             editText.setText(null)
+            Toast.makeText(applicationContext,"Canvas Cleared",Toast.LENGTH_SHORT).show()
+
         }
 
 
--- app/src/main/java/com/example/handwrittingrecognition/StrokeManager.kt
+++ app/src/main/java/com/example/handwrittingrecognition/StrokeManager.kt
@@ -3,17 +3,21 @@ package com.example.handwrittingrecognition
 import android.content.Context
 import android.util.Log
 import android.view.MotionEvent
+import android.widget.Toast
 import com.google.mlkit.common.MlKitException
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.common.model.RemoteModelManager
 import com.google.mlkit.vision.digitalink.*
-
+import com.example.handwrittingrecognition.MainActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.coroutines.coroutineContext
 
 
 object StrokeManager {
     private var inkBuilder = Ink.builder()
     private var strokeBuilder = Ink.Stroke.builder()
     private lateinit var model: DigitalInkRecognitionModel
+    public var lang : String = "en"
 
 
     fun addNewTouchEvent(event: MotionEvent) {
@@ -42,11 +46,11 @@ object StrokeManager {
         }
     }
 
-    fun download() {
+    fun download(context: Context) {
         var modelIdentifier: DigitalInkRecognitionModelIdentifier? = null
         try {
             modelIdentifier =
-                DigitalInkRecognitionModelIdentifier.fromLanguageTag("en") 
+                DigitalInkRecognitionModelIdentifier.fromLanguageTag(lang)
         } catch (e: MlKitException) {
             // language tag failed to parse, handle error.
         }
@@ -58,11 +62,18 @@ object StrokeManager {
         remoteModelManager.download(model, DownloadConditions.Builder().build())
             .addOnSuccessListener {
                 Log.i("StrokeManager", "Model downloaded")
+                Toast.makeText(context, "Model Downloaded", Toast.LENGTH_SHORT).show()
+
             }
             .addOnFailureListener { e: Exception ->
                 Log.e("StrokeManager", "Error while downloading a model: $e")
+                Toast.makeText(context, "Model Download Failed", Toast.LENGTH_SHORT).show()
             }
+
+
     }
+
+
 
     fun recognize(context: Context, callback: (String) -> Unit) {
         val recognizer: DigitalInkRecognizer =
